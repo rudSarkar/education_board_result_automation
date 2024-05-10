cc08c62acb2a99c2ee41f0e25596665e65676663
Rudra Sarkar <rudrasarkar815@gmail.com>
2024-05-10 19:24:44 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,43 @@
+This Python script automates the process of fetching exam results from the Bangladesh Education Board Results website using Playwright.
+
+### Requirements
+* Python 3.x
+* Playwright
+* dotenv
+
+### Installation
+
+1. Clone the repository:
+
+```
+https://github.com/rudSarkar/education_board_result_automation.git
+```
+
+2. Install dependencies:
+
+```
+pip install -r requirements.txt
+```
+
+### Usage
+
+1. Set up your environment variables in a `.env` file:
+```
+exam_name=ssc
+exam_year=2024
+exam_board=rajshahi
+exam_roll=612642
+exam_reg=1312650452
+```
+
+2. Run the script:
+
+```
+python main.py
+```
+
+This script will launch a browser in headless mode (to run invisibly in the background), navigate to the Bangladesh Education Board Results website, fill out the form with the provided credentials, calculate the sum of specific numbers, submit the form, and save the result as a PDF file named `result.pdf` in the current directory.If you want to run the script with the browser visible, you can set `headless` to `False` in the script.
+
+### Notes
+* The script will keep running until it successfully saves the PDF file.
+* Ensure that the provided credentials are correct and correspond to a valid examination result on the website.
---
+++ main.py
@@ -0,0 +1,51 @@
+import re
+import os
+from time import sleep
+from dotenv import load_dotenv, dotenv_values
+from playwright.sync_api import Playwright, sync_playwright, expect
+
+pdf_saved = False
+load_dotenv()
+
+def run(playwright: Playwright) -> None:
+    global pdf_saved
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("http://www.educationboardresults.gov.bd/")
+    page.locator("#exam").select_option(f"{os.getenv('exam_name')}")
+    page.locator("#year").select_option(f"{os.getenv('exam_year')}")
+    page.locator("#board").select_option(f"{os.getenv('exam_board')}")
+    page.locator("#roll").click()
+    page.locator("#roll").fill(f"{os.getenv('exam_roll')}")
+    page.locator("#roll").press("Tab")
+    page.locator("#reg").fill(f"{os.getenv('exam_reg')}")
+    
+    text = page.locator(
+        "body > table > tbody > tr:nth-child(2) > td > table > tbody > tr:nth-child(1) > td:nth-child(2) > table > tbody > tr:nth-child(2) > td > form > table > tbody > tr > td:nth-child(2) > fieldset > table > tbody > tr:nth-child(7) > td:nth-child(2)"
+    ).inner_text()
+
+    numbers = [int(num) for num in text.split('+')]
+    value_sum = sum(numbers)
+
+    page.locator("#value_s").click()
+    page.locator("#value_s").fill(f"{value_sum}")
+
+    page.get_by_role("button", name="Submit").click()
+
+    sleep(4)
+    page.pdf(path='result.pdf')
+
+    check_file = os.path.isfile("./result.pdf")
+    if check_file:
+        pdf_saved = True
+
+    # ---------------------
+    context.close()
+    browser.close()
+
+
+with sync_playwright() as playwright:
+    while not pdf_saved:
+        run(playwright)
+        
